75efac662fb36b7e970ca7850bdea4d3e3434b2e
ripki11 <ripkisaepullah70@gmail.com>
2023-01-11 10:33:05 +0700

    y

---
+++ plugins/sticker-ok.js
@@ -0,0 +1,21 @@
+import { sticker } from '../lib/sticker.js'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+
+let stiker = await sticker(null, global.API(`${pickRandom(stikerhuuu)}`), global.packname, global.author)
+    if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+    throw stiker.toString()
+    
+}
+
+handler.customPrefix = /^(ok|oke)$/i
+handler.command = new RegExp
+
+export default handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+let stikerhuuu = [
+ "https://telegra.ph/file/7ab5dc230fe9a6665185c.jpg",
+]
